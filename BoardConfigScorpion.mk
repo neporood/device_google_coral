7adf872da8a90730bc0ed317c95cf16c940b65d1
koen01 <koen.erens@gmail.com>
2022-02-02 12:54:39 +0000

    Revert "floral: Inline kernel building"
    This reverts commit 710b402cf7a3c5efb3b0e7644d45f9e735a52225.

--- BoardConfigScorpion.mk
+++ BoardConfigScorpion.mk
@@ -3,23 +3,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
-
-# Kernel
-BOARD_KERNEL_IMAGE_NAME := Image.lz4
-KERNEL_LD := LD=ld.lld
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC=$(shell pwd)/prebuilts/tools-lineage/$(HOST_OS)-x86/dtc/dtc \
-    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := floral_defconfig
-TARGET_KERNEL_SOURCE := kernel/google/coral
-TARGET_NEEDS_DTBOIMAGE := true
 
 # Partitions
 AB_OTA_PARTITIONS += \
