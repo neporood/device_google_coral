9b91c563a2856eaf21e0652805135fb2eb5ea3b7
koen01 <koen.erens@gmail.com>
2022-02-06 15:01:30 +0000

    Revert "Revert "floral: Inline kernel building""
    This reverts commit 356946054d77ca12813482cab016cee5a2bec600.

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -23,6 +23,18 @@ TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
 
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+KERNEL_LD := LD=ld.lld
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/tools-lineage/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := floral_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
+TARGET_NEEDS_DTBOIMAGE := true
 
 # Partitions
 AB_OTA_PARTITIONS += \
