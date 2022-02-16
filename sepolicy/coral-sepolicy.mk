8f3b162c53a0822d1c3a1631075a15b138722cec
koen01 <koen.erens@gmail.com>
2022-02-16 10:43:11 +0000

    coral: fix google battery denials

--- sepolicy/coral-sepolicy.mk
+++ sepolicy/coral-sepolicy.mk
@@ -16,3 +16,6 @@ BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 # system_ext
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/coral/sepolicy/system_ext/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/coral/sepolicy/system_ext/private
+
+# Battery
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
