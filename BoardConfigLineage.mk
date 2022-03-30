68dfecb6124655cfefa415367c6ae463e507c319
koen01 <koen.erens@gmail.com>
2022-03-30 15:01:56 +0000

    Revert "floral: Build LiveDisplay 2.0 HAL"
    This reverts commit f38b7bdf0f9de9993b51c46f502f6a885fbf64fa.

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -22,10 +22,6 @@ TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
 
-# Manifests
-DEVICE_MANIFEST_FILE += device/google/coral/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
-
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
@@ -40,10 +36,6 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 #endif
 #BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 30720000
 #BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 30720000
-
-# SELinux
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/dynamic
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
